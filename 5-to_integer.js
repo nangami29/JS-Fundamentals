74f2485a142704ed90f15212f76cdbfcdf042997
Nangami Rachael-Loureen <rachaelnangami29@gmail.com>
2025-05-22 13:26:44 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,12 @@
-const myNumber=process.argv[2];
-  
-// convert to integer since first argument is string
-const convertedNumber=parseInt(myNumber)
-if (isNaN(convertedNumber)){
-    console.log("Not a number");
-}else if(convertedNumber){console.log("My number: " + 89
-);}else{console.log("My number: " + -4545}
+
+
+const myNumber = process.argv[2];
+
+// Convert to integer
+const convertedNumber = parseInt(myNumber);
+
+if (isNaN(convertedNumber)) {
+  console.log("Not a number");
+} else {
+  console.log("My number: " + convertedNumber);
+}
