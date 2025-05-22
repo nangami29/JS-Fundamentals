d82080c165cf18b0627b6624bb55af4b7057d965
Nangami Rachael-Loureen <rachaelnangami29@gmail.com>
2025-05-22 14:28:21 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,8 +4,9 @@ x=parseInt(process.argv[2]);
 if (isNaN(x)){
     console.log('Missing number of occurrences');
 }else {
-    for(let i=0; i<x; i++){
-        console.log("C is fun");
-}
+   let i=0;
+    while(i<x){console.log('C is fun');
+              i++;
+              }
 
 }
