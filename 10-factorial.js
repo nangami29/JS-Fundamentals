87e4bd272400e85cbabb434e6f9bf3b19486aad9
nangami29 <rachaelnangami29@gmail.com>
2025-05-22 17:07:57 +0300

    advanced

---
+++ 10-factorial.js
@@ -0,0 +1,8 @@
+function factorial(n){
+    const arg=parseInt(Process.argv[2]);
+     if (isNaN(n) || n < 0) return 1;
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
