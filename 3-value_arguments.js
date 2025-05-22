2775bb82843768ea77caae2118e7cda4b8b2a965
nangami29 <rachaelnangami29@gmail.com>
2025-05-22 12:57:20 +0300

    2 arguments

--- 3-value_arguments.js
+++
@@ -1,8 +0,0 @@
- const firstArgument=process.argv[2]
-
- if (firstArgument===0){
-    console.log("No Argument");
- }else {
-    console.log("Argument found");
-
- }
---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Get the first and second arguments passed to the script
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+// Print in the format "firstArgument is secondArgument"
+console.log(firstArgument + " is " + secondArgument);
