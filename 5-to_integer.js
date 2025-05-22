1d018884808a9d7ba4ceef22e85ca4edc0bf24fb
nangami29 <rachaelnangami29@gmail.com>
2025-05-22 13:14:22 +0300

    first argument converted in integer

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const myNumber=process.argv[2];
+  
+// convert to integer since first argument is string
+const convertedNumber=parseInt(myNumber)
+if (isNaN(convertedNumber)){
+    console.log("Not a number");
+}else{console.log("My number:" + convertedNumber
+);}
