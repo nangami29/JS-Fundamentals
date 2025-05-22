14aac72d2a36cdfbb003f3ff4c28c636f3e87a5f
nangami29 <rachaelnangami29@gmail.com>
2025-05-22 16:08:40 +0300

    square

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const square=process.argv[2];
+let x=parseInt(square);
+if (isNaN(x)){
+    console.log('Missing size');
+}else{
+    let i = 0;
+    while(i<x){
+        console.log('x'.repeat(x));
+        i++;
+    }
+}
